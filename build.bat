0b54b1e569daff1247c03ff167b49a665a693265
Joe Biellik <joebiellik@gmail.com>
2014-12-18 17:47:01 +0000

    Update NuGet build environment

--- build.bat
+++ build.bat
@@ -5,8 +5,9 @@ if not "%PackageVersion%" == "" (
 	set version=-Version %PackageVersion%
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild SimpleRazor.sln /p:Configuration="Release-Net45" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild SimpleRazor.sln /p:Configuration="Release-Net40" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+mkdir build
 
-mkdir nuget
-cmd /c %nuget% pack "SimpleRazor\SimpleRazor.csproj" -symbols -o nuget -p Configuration=Release %version%
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild SimpleRazor.sln /p:Configuration="Release-Net45" /m /v:Q /fl /flp:LogFile=build/msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild SimpleRazor.sln /p:Configuration="Release-Net40" /m /v:Q /fl /flp:LogFile=build/msbuild.log;Verbosity=Normal /nr:false
+
+cmd /c %nuget% pack "SimpleRazor\SimpleRazor.nuspec" -symbols -o build %version% -verbosity detailed
